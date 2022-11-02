ee1d25db9faee9dc5598f73d44cb5ff3582c706b
Dr. Jose Edgar Lugo Castro <lugouadec@gmail.com>
2022-11-02 17:41:25 -0600

    Cambios
    cambios

---
+++ Programa01/src/programa01/Programa01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa01;
+
+/**
+ *
+ * @author LugoUAdeC
+ */
+public class Programa01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Como estas Esto es una prueba");
+        System.out.println("de Escritura de un programa java");
+    }
+    
+}
